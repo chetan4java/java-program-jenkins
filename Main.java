fe006dfeb70af7ff72d2cbf1f6fb4b709060fc4b
chetan4java <chetan.badgujar@gmail.com>
2024-09-23 17:30:23 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("X: This is the java program .. ");
+        System.out.println("Y: Output from java program .. ");
+        System.out.println("Date  " + new Date());
+    
+        
+    }
+    {
+
+    }
+}
